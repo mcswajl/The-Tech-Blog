5326299a7ed9a2168053f353f0d9b8c858456d3d
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-11-04 13:14:23 -0400

    wrote code for the timeout feature but its not working yet

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
-const helpers = require('./utils/helpers');
-
-const hbs = exphbs.create({ helpers });
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -24,6 +20,10 @@ const sess = {
 };
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,4 +34,29 @@ app.set('view engine', 'handlebars');
 app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
+
+    //time out in 30 sec on inactivity
+let session = new IdleSessionTimeout(3000);
+ 
+session.onTimeOut = () => {
+    async function logout() {
+        const response = await fetch('/api/users/logout', {
+          method: 'post',
+          headers: { 'Content-Type': 'application/json' }
+        });
+      
+        if (response.ok) {
+          document.location.replace('/');
+        } else {
+          alert(response.statusText);
+        }
+      }
+    console.log(logout);
+};
+ 
+// //optional
+// session.onTimeLeftChange = (timeLeft) => {
+//     // this will notify you  each second about the time left before the timeout
+//     console.log(`${timeLeft} ms left`);
+// }
 });
