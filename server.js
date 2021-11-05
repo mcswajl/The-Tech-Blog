9e8ab7b1161cf89cdfd67c345062542d251d3d49
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-11-05 11:38:39 -0400

    connect to jawsdb

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
